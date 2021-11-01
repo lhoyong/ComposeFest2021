0e909070e30a48a852ed951963744277b884d675
leehoyong <lee199402@gmail.com>
2021-11-02 00:25:45 +0900

    change theme name

--- week 1-Jetpack Compose basics/codelab1/app/src/main/java/com/github/lhoyong/codelab_week1/ui/theme/Theme.kt
+++ week 1-Jetpack Compose basics/codelab1/app/src/main/java/com/github/lhoyong/codelab_week1/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun Codelabweek1Theme(
+fun CodelabWeek1Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
