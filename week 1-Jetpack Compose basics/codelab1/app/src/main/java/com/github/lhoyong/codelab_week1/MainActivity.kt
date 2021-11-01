aa0113d3b1c4c4e2c8b690609f435a677c0ba7d9
leehoyong <lee199402@gmail.com>
2021-11-02 00:38:06 +0900

    basic 9. Creating a performant lazy list

--- week 1-Jetpack Compose basics/codelab1/app/src/main/java/com/github/lhoyong/codelab_week1/MainActivity.kt
+++ week 1-Jetpack Compose basics/codelab1/app/src/main/java/com/github/lhoyong/codelab_week1/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
@@ -44,9 +46,9 @@ fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
